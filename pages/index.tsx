0cd376ef2b05e6c99ce8ac71b50043c7a7092b8e
Jacopo <jacopocontin1986@gmail.com>
2023-02-14 18:01:27 +0100

    deleted question

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function IndexPage() {
       <Head>
         <title>Deecide</title>
         <meta
-          name="description"
+          name="Deecide"
           content="A decision making tool, for people who just can't make up their mind."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -27,9 +27,16 @@ export default function IndexPage() {
             Don't know whether to eat pizza or sushi tonight? You're in luck.
           </p>
         </div>
-        <div className="flex gap-4">
-          <Button className="" variant="" size="lg" >
-            Try me
+        <div className="flex gap-4 mx-auto">
+          <input 
+            type="email"
+            name="email"
+            id="email"
+            className="block w-full px-4 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            placeholder="Write your question here..."
+          ></input>
+          <Button className="" variant="default" size="lg" >
+            Try
           </Button> 
         </div>
       </section>
